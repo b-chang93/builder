2e0339c17dfacf39816c0d49d70de7a48b641108
bchang-93 <bdc11122@gmail.com>
2018-10-25 16:54:22 -0700

    remove commented out code

--- config.js
+++ config.js
@@ -1,17 +1,5 @@
 "use strict";
 
-// exports.DATABASE_URL =
-//   process.env.DATABASE_URL || "mongodb://localhost/workout-app";
-//
-// // exports.TEST_BUILDR_DATABASE =
-// //   process.env.TEST_BUILDR_DATABASE || "mongodb://localhost/workout-test-app";
-// exports.TEST_BUILDR_DATABASE =
-//   process.env.TEST_BUILDR_DATABASE || "mongodb://localhost/buildr-test-db";
-//
-// exports.PORT = process.env.PORT || 8080;
-// exports.JWT_SECRET = process.env.JWT_SECRET;
-// exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
-
 module.exports = {
   PORT: process.env.PORT || 8080,
   DATABASE_URL: process.env.DATABASE_URL || "mongodb://localhost/workout-app",
