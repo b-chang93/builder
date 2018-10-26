305785668a438ca0cdf2bc152c540475c7cd6144
bchang-93 <bdc11122@gmail.com>
2018-10-26 01:39:54 -0700

    clean up config file and add env to gitignore

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  DATABASE_URL: process.env.DATABASE_URL || "mongodb://localhost/workout-app",
+  DATABASE_URL: process.env.DATABASE_URL || "mongodb://localhost/buildrr-db",
   TEST_BUILDR_DATABASE: process.env.TEST_BUILDR_DATABASE || "mongodb://localhost/buildr-test-db",
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || "7d"
